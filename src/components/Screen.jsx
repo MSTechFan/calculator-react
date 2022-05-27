c36ae3f7b793a97f3cd57d436984e6cd914772ab
Mario Andres Suarez Bravo -X (msuarezb) <msuarezb@cisco.com>
2022-05-27 17:29:21 -0500

    finishing styles

--- src/components/Screen.jsx
+++ src/components/Screen.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Screen = (props) => {
   return (
-    <div style={{flex: 1, textAlign: "end", padding: "5px", fontSize: "2.5rem"}}>{props.result}</div>
+    <div style={{flex: 1, textAlign: "end", padding: "5px", fontSize: "2.5rem"}}>
+      <input type={'number'} style={{all: 'none', backgroundColor: '#2E0249', color: 'white'}}></input>
+      {/* crear un estado para saber si se está escribiendo o no, para poder dejar la calculadora en 0 */}
+    </div>
   )
 }
 
