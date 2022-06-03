aa7153de07431c31b7dd749556578af33ca172d7
Mario Andres Suarez Bravo -X (msuarezb) <msuarezb@cisco.com>
2022-06-03 15:00:05 -0500

    messahes

--- src/components/Keyboard.jsx
+++ src/components/Keyboard.jsx
@@ -7,18 +7,18 @@ const Keyboard = (props) => {
         
         {/* first line */}  
         <ButtonLarge onClick={() => props.setScreenResult("")}>AC</ButtonLarge>
-        <ButtonEje onClick={() => props.setScreenResult("+")}>+</ButtonEje>
-        <ButtonEje onClick={() => props.setScreenResult("-")}>-</ButtonEje>
+        <ButtonEje onClick={() => props.setScreenResult("")}>+</ButtonEje>
+        <ButtonEje onClick={() => props.setScreenResult("")}>-</ButtonEje>
         {/* second line */}
         <ButtonEje onClick={() => props.setScreenResult(1)}>1</ButtonEje>
         <ButtonEje onClick={() => props.setScreenResult(2)}>2</ButtonEje>
         <ButtonEje onClick={() => props.setScreenResult(3)}>3</ButtonEje>
-        <ButtonEje onClick={() => props.setScreenResult("*")}>x</ButtonEje>
+        <ButtonEje onClick={() => props.setScreenResult("")}>x</ButtonEje>
         {/* third line */}
         <ButtonEje onClick={() => props.setScreenResult(4)}>4</ButtonEje>
         <ButtonEje onClick={() => props.setScreenResult(5)}>5</ButtonEje>
         <ButtonEje onClick={() => props.setScreenResult(6)}>6</ButtonEje>
-        <ButtonEje onClick={() => props.setScreenResult("/")}>/</ButtonEje>
+        <ButtonEje onClick={() => props.setScreenResult("")}>/</ButtonEje>
         {/* fourth line */}
         <ButtonEje onClick={() => props.setScreenResult(7)}>7</ButtonEje>
         <ButtonEje onClick={() => props.setScreenResult(7)}>8</ButtonEje>
